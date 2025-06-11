c87462ffcaac596d65e5c4cb0e5275444922acd2
Santiago Alexis Bucheli Dorado <SANTYBUCHELI@gmail.com>
2025-06-11 01:33:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,9 +1,10 @@
+
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
-  ({className, ...props}, ref) => {
+  ({className, value: propValue, ...props}, ref) => {
     return (
       <textarea
         className={cn(
@@ -11,6 +12,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
           className
         )}
         ref={ref}
+        value={propValue === null || propValue === undefined ? '' : String(propValue)}
         {...props}
       />
     );
